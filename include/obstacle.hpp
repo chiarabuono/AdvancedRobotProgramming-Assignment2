d9425d0ed03ed26ee7ada54bdcc8bae429737281
Chiara Buono <s7687956@studenti.unige.it>
2025-02-13 17:48:50 +0100

    adjustment

--- include/obstacle.hpp
+++ include/obstacle.hpp
@@ -1,10 +1,11 @@
 #ifndef OBSTACLE_HPP
 #define OBSTACLE_HPP
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <csignal>
 #include "auxfunc2.hpp"
 
 // Macro di configurazione
@@ -14,86 +15,80 @@
 // Variabili globali
 extern FILE *obstFile;
 
-#define LOGNEWMAP(status) {                                                      \
-    if (!obstFile) {                                                              \
-        perror("Log file not initialized.\n");                                   \
-        raise(SIGTERM);                                                                  \
-    }                                                                            \
-                                                                                 \
-    char date[50];                                                               \
-    getFormattedTime(date, sizeof(date));                                        \
-                                                                                 \
-    fprintf(obstFile, "%s New obstacle generated.\n", date);                             \
-    for (int t = 0; t < MAX_OBSTACLES; t++) {                                       \
-        fprintf(obstFile, "(%d, %d) ",                                  \
-                status.obstacles.x[t], status.obstacles.y[t]); \
-    }                                                                            \
-    fprintf(obstFile, "\n");                                                      \
-    fflush(obstFile);                                                             \
-}
+// Macro per il logging
+#define LOGNEWMAP(status) \
+    if (!obstFile) { \
+        perror("Log file not initialized.\n"); \
+        raise(SIGTERM); \
+    } \
+    char date[50]; \
+    getFormattedTime(date, sizeof(date)); \
+    fprintf(obstFile, "%s New obstacle generated.\n", date); \
+    for (int t = 0; t < MAX_OBSTACLES; t++) { \
+        fprintf(obstFile, "(%d, %d) ", status.obstacles.x[t], status.obstacles.y[t]); \
+    } \
+    fprintf(obstFile, "\n"); \
+    fflush(obstFile); \
+    
+
+#define LOGPROCESSDIED() \
+    if (!obstFile) { \
+        perror("Log file not initialized.\n"); \
+        raise(SIGTERM); \
+    } \
+    char date[50]; \
+    getFormattedTime(date, sizeof(date)); \
+    fprintf(obstFile, "%s Process dead. Obstacle is quitting\n", date); \
+    fflush(obstFile); \
 
 
-#define LOGPROCESSDIED() { \
-    if (!obstFile) {                                                              \
-        perror("Log file not initialized.\n");                                   \
-        raise(SIGTERM);                                                                  \
-    }                                                                            \
-                                                                                 \
-    char date[50];                                                               \
-    getFormattedTime(date, sizeof(date));                                           \
-    fprintf(obstFile, "%s Process dead. Obstacle is quitting\n", date);                              \
-    fflush(obstFile);                                                             \
-}
-
-#define LOGSUBSCRIPTION(current_count_change) {
-    if (!obstFile) {                                                              \
-        perror("Log file not initialized.\n");                                   \
-    }\
-    char date[50];                                                               \
+#define LOGSUBSCRIPTION(current_count_change) \
+    if (!obstFile) { \
+        perror("Log file not initialized.\n"); \
+        raise(SIGTERM); \
+    } \
+    char date[50]; \
     getFormattedTime(date, sizeof(date)); \
     if (current_count_change == 1) { \
         fprintf(obstFile, "%s Subscription matched\n", date); \
-    } else if (current_count_change == -1) {    \
-        fprintf(tagFile, "%s Subscription un-matched\n", date); \
+    } else if (current_count_change == -1) { \
+        fprintf(obstFile, "%s Subscription un-matched\n", date); \
     } else { \
-        fprintf(obstFile, "%s %d is not a valid value for SubscriptionMatchedStatus current count change\n", date, current_count); \
+        fprintf(obstFile, "%s %d is not a valid value\n", date, current_count_change); \
     } \
-    fflush(obstFile); \    
-}
+    fflush(obstFile); \
 
-
-#define LOGPUBLISHERMATCHING(current_count_change) {
-    if (!obstFile) {                                                              \
-        perror("Log file not initialized.\n");                                   \
-    }\
-    char date[50];                                                               \
+#define LOGPUBLISHERMATCHING(current_count_change) \
+    if (!obstFile) { \
+        perror("Log file not initialized.\n"); \
+    } \
+    char date[50]; \
     getFormattedTime(date, sizeof(date)); \
     if (current_count_change == 1) { \
         fprintf(obstFile, "%s Obstacle Publisher matched\n", date); \
-    } else if (current_count_change == -1) {    \
-        fprintf(tagFile, "%s Obstacle Publisher unmatched.\n", date); \
+    } else if (current_count_change == -1) { \
+        fprintf(obstFile, "%s Obstacle Publisher unmatched.\n", date); \
     } else { \
-        fprintf(obstFile, "%s %d is not a valid value for PublicationMatchedStatus current count change\n", date, current_count); \
+        fprintf(obstFile, "%s %d is not a valid value\n", date, current_count_change); \
     } \
-    fflush(obstFile); \    
-}
+    fflush(obstFile); \
+
 #if USE_DEBUG
-#define LOGPUBLISHNEWTARGET(obstacles) {     \
-    if (!obstFile) {                                                              \
-        perror("Log file not initialized.\n");                                   \
-        }\
+#define LOGPUBLISHNEWTARGET(obstacles) {\
+    if (!obstFile) { \
+        perror("Log file not initialized.\n"); \
+    } \
     char date[50]; \
     getFormattedTime(date, sizeof(date)); \
     fprintf(obstFile, "%s Obstacle published correctly:\n", date); \
-    for (int t = 0; t < obstacles.obstacles_number(); t++) {                                       \
-        fprintf(obstFile, "(%d, %d) ", obstacles_x()[t], obstacles_y()[t]); \
-    }                                                                            \
-    fprintf(obstFile, "\n");                                                      \
-    fflush(obstFile);                                                             \
+    for (int t = 0; t < obstacles.obstacles_number(); t++) { \
+        fprintf(obstFile, "(%d, %d) ", obstacles.x()[t], obstacles.y()[t]); \
+    } \
+    fprintf(obstFile, "\n"); \
+    fflush(obstFile); \
 }
 #else
-#define LOGPUBLISHNEWTARGET(obstacles) {}
+#define LOGPUBLISHNEWTARGET(obstacles) 
 #endif
-
 
 #endif // OBSTACLE_HPP
--- include/target.hpp
+++ include/target.hpp
@@ -14,7 +14,7 @@
 // Variabili globali
 extern FILE *targFile;
 
-#define LOGNEWMAP(status) {                                                      \
+#define LOGNEWMAP(target) {                                                      \
     if (!targFile) {                                                              \
         perror("Log file not initialized.\n");                                   \
         raise(SIGTERM);                                                                  \
@@ -24,9 +24,9 @@ extern FILE *targFile;
     getFormattedTime(date, sizeof(date));                                        \
                                                                                  \
     fprintf(targFile, "%s New target generated.\n", date);                             \
-    for (int t = 0; t < MAX_TARGET; t++) {                                       \
+    for (int t = 0; t < target.number; t++) {                                       \
         fprintf(targFile, "(%d, %d) ",                                  \
-                status.targets.x[t], status.targets.y[t]); \
+                targets.x[t], targets.y[t]); \
     }                                                                            \
     fprintf(targFile, "\n");                                                      \
     fflush(targFile);                                                             \
@@ -45,38 +45,40 @@ extern FILE *targFile;
     fflush(targFile);                                                             \
 }
 
-#define LOGSUBSCRIPTION(current_count_change) {
-    if (!targFile) {                                                              \
-        perror("Log file not initialized.\n");                                   \
-    }\
-    char date[50];                                                               \
+#define LOGSUBSCRIPTION(current_count_change) { \
+    if (!targFile) { \
+        perror("Log file not initialized.\n"); \
+        raise(SIGTERM); \
+    } \
+    char date[50]; \
     getFormattedTime(date, sizeof(date)); \
     if (current_count_change == 1) { \
         fprintf(targFile, "%s Subscription matched\n", date); \
-    } else if (current_count_change == -1) {    \
-        fprintf(tagFile, "%s Subscription un-matched\n", date); \
+    } else if (current_count_change == -1) { \
+        fprintf(targFile, "%s Subscription un-matched\n", date); \
     } else { \
-        fprintf(targFile, "%s %d is not a valid value for SubscriptionMatchedStatus current count change\n", date, current_count); \
+        fprintf(targFile, "%s %d is not a valid value for SubscriptionMatchedStatus current count change\n", date, current_count_change); \
     } \
-    fflush(targFile); \    
+    fflush(targFile); \
 }
 
-
-#define LOGPUBLISHERMATCHING(current_count_change) {
-    if (!targFile) {                                                              \
-        perror("Log file not initialized.\n");                                   \
-    }\
-    char date[50];                                                               \
+#define LOGPUBLISHERMATCHING(current_count_change) { \
+    if (!targFile) { \
+        perror("Log file not initialized.\n"); \
+        raise(SIGTERM); \
+    } \
+    char date[50]; \
     getFormattedTime(date, sizeof(date)); \
     if (current_count_change == 1) { \
-        fprintf(targFile, "%s Target Publisher matched\n", date); \
-    } else if (current_count_change == -1) {    \
-        fprintf(tagFile, "%s Target Publisher unmatched.\n", date); \
+        fprintf(targFile, "%s Publisher matched\n", date); \
+    } else if (current_count_change == -1) { \
+        fprintf(targFile, "%s Publisher un-matched\n", date); \
     } else { \
-        fprintf(targFile, "%s %d is not a valid value for PublicationMatchedStatus current count change\n", date, current_count); \
+        fprintf(targFile, "%s %d is not a valid value for PublicationMatchedStatus current count change\n", date, current_count_change); \
     } \
-    fflush(targFile); \    
+    fflush(targFile); \
 }
+
 #if USE_DEBUG
 #define LOGPUBLISHNEWTARGET(targets) {     \
     if (!targFile) {                                                              \
@@ -86,7 +88,7 @@ extern FILE *targFile;
     getFormattedTime(date, sizeof(date)); \
     fprintf(targFile, "%s Target published correctly:\n", date); \
     for (int t = 0; t < targets.targets_number(); t++) {                                       \
-        fprintf(targFile, "(%d, %d) ", targets_x()[t], targets_y()[t]); \
+        fprintf(targFile, "(%d, %d) ", targets.targets_x()[t], targets.targets_y()[t]); \
     }                                                                            \
     fprintf(targFile, "\n");                                                      \
     fflush(targFile);                                                             \
@@ -96,4 +98,4 @@ extern FILE *targFile;
 #endif
 
 
-#endif // TARGET_H
+#endif // TARGET_HPP
